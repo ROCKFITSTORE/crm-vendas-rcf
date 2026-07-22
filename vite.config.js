639aa91ccd1d929c39e581e82dcad5f38b373d83
ROCKFITSTORE <vendas@rockfitstore.com.br>
2026-07-22 12:59:43 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/crm-vendas-rockfit/", // caminho do repositório no GitHub Pages
+  base: "/crm-vendas-rcf/", // caminho do repositório no GitHub Pages
 });
 
